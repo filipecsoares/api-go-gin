a0cf85df55f45df83f294a868ead982e24da068a
Filipe Soares <filipecarlossoares@gmail.com>
2022-12-10 21:31:03 -0300

    create student struct

--- controllers/controller.go
+++ controllers/controller.go
@@ -3,12 +3,10 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/filipecsoares/api-go-gin/models"
 	"github.com/gin-gonic/gin"
 )
 
 func GetAllStudents(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"id":   1,
-		"name": "Filipe",
-	})
+	c.JSON(http.StatusOK, models.Students)
 }
--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "github.com/filipecsoares/api-go-gin/routes"
+import (
+	"github.com/filipecsoares/api-go-gin/models"
+	"github.com/filipecsoares/api-go-gin/routes"
+)
 
 func main() {
+	models.Students = []models.Student{
+		{Name: "Filipe", Id: 1, Email: "filipe@email.com"},
+		{Name: "Tumo", Id: 2, Email: "tumo@email.com"},
+		{Name: "Tuminho", Id: 3, Email: "tuminho@email.com"},
+		{Name: "Mae deles", Id: 4, Email: "maedeles@email.com"},
+	}
 	routes.HandleRequests()
 }
