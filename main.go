84051e18871f8d56c77fb12fa0c247ce978305b7
Filipe Soares <filipecarlossoares@gmail.com>
2022-12-10 21:14:28 -0300

    separate modules

--- main.go
+++ main.go
@@ -1,20 +1,7 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-)
-
-func GetAllStudents(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"id":   1,
-		"name": "Filipe",
-	})
-}
+import "github.com/filipecsoares/api-go-gin/routes"
 
 func main() {
-	r := gin.Default()
-	r.GET("/students", GetAllStudents)
-	r.Run()
+	routes.HandleRequests()
 }
---
+++ routes/routes.go
@@ -0,0 +1,12 @@
+package routes
+
+import (
+	"github.com/filipecsoares/api-go-gin/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func HandleRequests() {
+	r := gin.Default()
+	r.GET("/students", controllers.GetAllStudents)
+	r.Run()
+}
