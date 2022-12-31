81104a7bd0b8477e0ee9c64b1342c3f6e58c27ef
Filipe Soares <filipecarlossoares@gmail.com>
2022-12-31 19:01:25 -0300

    update and find by email student

--- controllers/controller.go
+++ controllers/controller.go
@@ -27,6 +27,19 @@ func GetStudentById(c *gin.Context) {
 	c.JSON(http.StatusOK, student)
 }
 
+func GetStudentByEmail(c *gin.Context) {
+	var student models.Student
+	email := c.Param("email")
+	database.DB.Where(&models.Student{Email: email}).First(&student)
+	if student.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "Student not found",
+		})
+		return
+	}
+	c.JSON(http.StatusOK, student)
+}
+
 func CreateStudent(c *gin.Context) {
 	var student models.Student
 	if err := c.ShouldBindJSON(&student); err != nil {
@@ -46,4 +59,18 @@ func DeleteStudent(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Student deleted",
 	})
+}
+
+func EditStudent(c *gin.Context) {
+	var student models.Student
+	id := c.Params.ByName("id")
+	database.DB.First(&student, id)
+	if err := c.ShouldBindJSON(&student); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	database.DB.Model(&student).UpdateColumns(student)
+	c.JSON(http.StatusOK, student)
 }
--- routes/routes.go
+++ routes/routes.go
@@ -9,7 +9,9 @@ func HandleRequests() {
 	r := gin.Default()
 	r.GET("/students", controllers.GetAllStudents)
 	r.GET("/students/:id", controllers.GetStudentById)
+	r.GET("/students/email/:email", controllers.GetStudentByEmail)
 	r.POST("/students", controllers.CreateStudent)
 	r.DELETE("/students/:id", controllers.DeleteStudent)
+	r.PATCH("/students/:id", controllers.EditStudent)
 	r.Run()
 }
