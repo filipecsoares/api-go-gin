a901e8f480bec907e365c921f14bf1625e7b54a2
filipecsoares <filipecarlossoares@gmail.com>
2023-06-20 14:01:33 -0300

    feat: validate student model

--- models/student.go
+++ models/student.go
@@ -1,10 +1,20 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gopkg.in/validator.v2"
+	"gorm.io/gorm"
+)
 
 type Student struct {
 	gorm.Model
 	Id    int    `json:"id"`
-	Name  string `json:"name"`
-	Email string `json:"email"`
+	Name  string `json:"name" validate:"nonzero"`
+	Email string `json:"email" validate:"min=3,max=80"`
+}
+
+func (s *Student) Validate() error {
+	if err := validator.Validate(s); err != nil {
+		return err
+	}
+	return nil
 }
